3af39ce406e510ad045888094595e4acf604f88a
rahul_motwani <rahul_motwani@persistent.com>
2020-08-03 15:11:55 +0530

    Updates

---
+++ precache-manifest.7fc40618909d9c8ef260525acccdae22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0f601552238553f986210d571c85e",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "536604ac565b8374e2da",
+    "url": "/recipeapp/static/css/main.4dbe1eed.chunk.css"
+  },
+  {
+    "revision": "91c31cd3dd34ee4d5748",
+    "url": "/recipeapp/static/js/2.eacdf52e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/recipeapp/static/js/2.eacdf52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536604ac565b8374e2da",
+    "url": "/recipeapp/static/js/main.e14d201f.chunk.js"
+  },
+  {
+    "revision": "2c6823dfb356579ecc04",
+    "url": "/recipeapp/static/js/runtime-main.83b6e7d9.js"
+  }
+]);
